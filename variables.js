e22ace4dd202e7452ca8a222aef6eb555c3e1f03
NagayasashwiniNagarajan <101091516+NagayasashwiniNagarajan@users.noreply.github.com>
2022-03-09 18:35:54 +0530

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,21 @@
+let name = "yash"; //declaring variable
+let _lastName = "ashwini";
+let $myNum = 5;
+
+const price_f = 2;  // declaring constant variable
+const price_s = 8;
+let total = price_f + price_s;
+
+let firstName = "John";
+firstName = "Jezy";
+
+const number = 6;
+//number = 8;
+
+console.log(number);
+console.log(firstName);
+console.log(total);
+console.log(name); 
+
+
+
