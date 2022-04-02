66fd8e65592f36b250b7dc52a5b0634d1e6a5ae7
NagayasashwiniNagarajan <101091516+NagayasashwiniNagarajan@users.noreply.github.com>
2022-04-02 15:00:28 +0530

    Add files via upload

---
+++ arrayMethod.js
@@ -0,0 +1,36 @@
+function validate(input)
+{
+    if(Array.isArray(input))
+    {
+        var isInteger = input.every(function(elements){
+            return elements>0;
+        });
+        console.log(`Array integer check :${isInteger}`);
+
+        if(isInteger){
+            var sum = input.reduce(function(accumulator,currentValue){
+                return accumulator + currentValue;
+            });
+            console.log(`The sum of positive array elements : ${sum}`);
+            return sum;
+        }
+        else{
+            return "Invalid Input";
+        }
+    }
+}
+console.log(validate([1,2,3]));
+
+function validate(input){
+    if(Array.isArray(input)){
+        var isInteger = input.every((elements)=>(elements>0));
+        if(isInteger){
+            var sum = input.reduce((accumulator,currentValue)=>(accumulator + currentValue));
+            return sum;
+        }
+        else{
+            return "Invalid Input";
+        }
+    }
+}
+console.log(validate([1,-2,3,4]));
